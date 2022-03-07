8022b4b9ebf1877a9d1b75ce2449e4d3437320c1
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-03-07 20:48:19 +0600

    first commit

---
+++ datd_types_and_input_output.c
@@ -0,0 +1,64 @@
+/* #include<stdio.h>
+
+int main(){
+    int a;
+    int b;
+    int sum;
+    a = 50;
+    b = 60;
+    sum = a + b;
+    printf("sum is %d\n", sum);
+    return 0;
+} */
+
+
+/* #include<stdio.h>
+
+int main(){
+    int x, y;
+    x = 1;
+    y = x;
+    x = 2;
+    printf("%d", y);
+    return 0;
+} */
+
+
+/* #include<stdio.h>
+
+int main(){
+    int x, y, sum;
+    x = 60;
+    y = 50;
+    sum = x + y;
+    printf("%d + %d = %d",  y, x, sum);
+    return 0;
+} */
+
+/* #include<stdio.h>
+
+int main(){
+    int x, y, sum;
+    x = 60.75;
+    y = 50;
+    sum = x + y;
+    printf("%d + %d = %d",  y, x, sum);
+    return 0;
+} */
+
+/* #include<stdio.h>
+
+int main(){
+    double x , y, sum;
+
+    printf("Enter the double number X :\n");
+    scanf("%lf", &x);
+    printf("Enter the double number Y :\n");
+    scanf("%lf", &y);
+
+    sum = x + y;
+    printf("%.2lf + %.2lf = %.2lf", x, y, sum);
+    return 0;
+} */
+
+
